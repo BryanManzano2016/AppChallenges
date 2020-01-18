59e253cd97f2c1a48f74d4b6846117b969dfe67e
ltvargas <ltvargas@espol.edu.ec>
2020-01-18 18:23:59 -0500

    fragmentar app (part 1)

---
+++ FrontEnd/Tester/app/src/main/java/com/example/actividades/frag_home.kt
@@ -0,0 +1,13 @@
+package com.example.actividades
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.actividades.R
+
+class frag_home : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_frag_home)
+    }
+}
---
+++ FrontEnd/Tester/app/src/main/java/com/example/actividades/principal.kt
@@ -0,0 +1,28 @@
+package com.example.actividades
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.google.android.material.bottomnavigation.BottomNavigationView
+
+
+class principal : AppCompatActivity() {
+    /*
+}
+    private val navListener: BottomNavigationView.OnNavigationItemSelectedListener = BottomNavigationView.OnNavigationItemSelectedListener {
+        item->
+            when (item.getItemId()) {
+                R.id.nav_home -> { message.settext()    selectedFragment = HomeFragment()
+                R.id.nav_favorites -> selectedFragment = FavoritesFragment()
+                R.id.nav_search -> selectedFragment = SearchFragment()
+            }
+            supportFragmentManager.beginTransaction().replace(R.id.fragment_container,
+                    selectedFragment).commit()
+            return true
+        }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_principal)
+
+    }*/
+}
