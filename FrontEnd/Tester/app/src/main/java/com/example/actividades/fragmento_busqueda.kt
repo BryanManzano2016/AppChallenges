b985d69cbf78d90010ad3cb5ccde32ef421a81b2
jose598 <morales2010mendoza@hotmail.com>
2020-01-19 16:49:45 -0500

    tablelayout

--- FrontEnd/Tester/app/src/main/java/com/example/actividades/fragmento_busqueda.kt
+++ FrontEnd/Tester/app/src/main/java/com/example/actividades/fragmento_busqueda.kt
@@ -6,8 +6,19 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageButton
+import android.widget.ImageView
 
 import com.example.actividades.R
+import com.example.clases.Auxiliar
+import com.example.clases.Challenge
+import kotlinx.android.synthetic.main.activity_frag_busqueda.*
+import kotlinx.android.synthetic.main.activity_principal.*
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.withContext
+import org.json.JSONArray
+import java.util.*
+import kotlin.collections.HashMap
 
 /**
  * A simple [Fragment] subclass.
@@ -19,6 +30,44 @@ class fragmento_busqueda : Fragment() {
 
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.activity_frag_busqueda, container, false)
+    }
+
+
+    private suspend fun CargarChallengesDestacados() {
+        val solicitud = obtenerChallengesBusqueda()
+        withContext(Dispatchers.Main) {
+            when (Auxiliar().mensajeServidor(solicitud)) {
+                0 -> print("0")
+                -1 -> {
+                    val listaChallenge = LinkedList<Challenge>()
+                    val urls = LinkedList<String>()
+                    val nombres = LinkedList<String>()
+                    val mapa=HashMap<ImageButton,Challenge>()
+                    for (i in 0 until solicitud.length()) {
+                        listaChallenge.add(Auxiliar().objectoChallenge(solicitud.getJSONObject(i)))
+                    }
+
+                    mapa.put(imageButton2,listaChallenge[0])
+                    
+                    Auxiliar().colocarImagen(Challenge1, urls.first, editText, nombres.first)
+                    Auxiliar().colocarImagen(challenge3, urls.get(1), editText2, nombres.get(1))
+                    Auxiliar().colocarImagen(challenge5, urls.get(2), editText3, nombres.get(2))
+
+                }
+            }
+        }
+    }
+
+    private suspend fun obtenerChallengesBusqueda(): JSONArray {
+        return withContext(Dispatchers.Default) {
+            val solicitud = Auxiliar().solicitudHttpGet(
+                    Auxiliar().obtener_Ip() + "obtenerChallenges"
+
+            )
+
+            val respuesta = Auxiliar().respuestaString(solicitud.body())
+            return@withContext JSONArray(respuesta)
+        }
     }
 
 
